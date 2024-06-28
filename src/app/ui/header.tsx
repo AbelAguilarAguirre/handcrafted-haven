bf5fe3f40db03fac47866769bcf220afaba32956
Abel Aguilar <ulysses.aguilar.aguirre@gmail.com>
2024-06-27 14:09:28 -1000

    styled header

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Nav from './nav';
+import React from "react";
+import Nav from "./nav";
 
 export default function Header() {
     return (
-        <header className='flex justify-between p-10'>
-            <h1>Handcrafted Haven</h1>
+        <header className="flex flex-wrap justify-around p-10 items-center">
+            <h1 className="text-4xl font-bold text-gray-800">
+                Handcrafted Haven
+            </h1>
             <Nav />
         </header>
     );
-};
+}
