1d40f2f2e1b6ea26cc5b7e84940f5922f790f024
natelandrum <nate178dawg@gmail.com>
2024-06-27 19:05:37 -0600

    Add hero, features, and subscribe components to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Hero from "./ui/hero";
+import Features from "./ui/features";
+import Subscribe from "./ui/subscribe";
 
 export default function Home() {
   return (
     <main>
-      
+      <Hero />
+      <Features />
+      <Subscribe />
     </main>
   );
 }
