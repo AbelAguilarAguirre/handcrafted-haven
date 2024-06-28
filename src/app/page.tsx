2227b99170688d15c7c067fced33d5e61f634558
natelandrum <nate178dawg@gmail.com>
2024-06-28 13:53:37 -0600

    Move Header component to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Header from "./ui/header";
 import Hero from "./ui/hero";
 import Features from "./ui/features";
 import Subscribe from "./ui/subscribe";
@@ -7,7 +6,6 @@ import Subscribe from "./ui/subscribe";
 export default function Home() {
   return (
     <main>
-      <Header />
       <Hero />
       <Features />
       <Subscribe />
