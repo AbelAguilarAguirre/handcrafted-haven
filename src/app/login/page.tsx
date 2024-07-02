5b6069d85100d1b8036015e8aa8f00cf30aca6eb
Abel Aguilar <ulysses.aguilar.aguirre@gmail.com>
2024-07-02 00:47:18 -1000

    Login page is done link to login page from nav complete. login form made. CSS needs work

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../ui/login-form";
+
+export default function LoginPage() {
+    return (
+        <div className="container pt-16 pb-4 px-8 mx-auto text-center">
+            <LoginForm />
+        </div>
+    );
+}
---
+++ src/app/ui/login-form.tsx
@@ -0,0 +1,23 @@
+export default function LoginForm() {
+    return (
+            <form className="flex flex-col gap-x-8 text-lg flex" >
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="Enter Email Address"
+                    required
+                    className="w-full mb-2 px-2 md:py-0 pl-4 md:my-0 md:w-96 border-solid border-blue-700 border-2 rounded-lg focus:bg-blue-50 focus:outline-none"
+                />
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    required
+                    className="w-full mb-2 px-2 md:py-0 pl-4 md:my-0 md:w-96 border-solid border-blue-700 focus:bg-blue-50 focus:outline-none border-2 rounded-lg"
+                />
+                <button className="w-full md:w-96 py-2 px-2 bg-blue-700 text-white rounded-lg cursor-pointer" type="submit">Login</button>
+            </form>
+    );
+}
