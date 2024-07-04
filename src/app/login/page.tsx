ba4422ed4082a6468f5e5ad10f87a5b9b5302348
natelandrum <nate178dawg@gmail.com>
2024-07-03 19:24:35 -0600

    Change Page back to LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 import LoginForm from "../ui/login-form";
 
-export default function Page() {
+export default function LoginPage() {
     return (
         <div className="container pb-8 px-8 mx-auto text-center">
             <LoginForm />
