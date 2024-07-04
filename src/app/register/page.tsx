d67ee469ebc41b63b9f1a32bbff3bf74511c1f50
natelandrum <nate178dawg@gmail.com>
2024-07-03 19:24:49 -0600

    Change Page to RegisterPage

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 import RegisterForm from "../ui/register-form";
 
-export default function Page() {
+export default function RegisterPage() {
     return (
         <div className="container pb-8 px-8 mx-auto text-center">
             <RegisterForm />
