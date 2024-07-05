aab776a126167dd5790920d37a96370a542ca723
Abel Aguilar <ulysses.aguilar.aguirre@gmail.com>
2024-07-05 01:55:43 -1000

    Created dynamic page that gets the profile info of a user id

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,24 @@
+export type Product = {
+    id: number;
+    name: string;
+    rating: number;
+    price: number;
+    description: string;
+    image: string;
+    user_id: number;
+    created_at: Date;
+    updated_at: Date;
+};
+
+
+export type ProductTable = {
+    id: number;
+    name: string;
+    rating: number;
+    price: number;
+    description: string;
+    image: string;
+    user_id: number;
+    created_at: Date;
+    updated_at: Date;
+};
