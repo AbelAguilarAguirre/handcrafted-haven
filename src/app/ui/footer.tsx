14c6f6aa74323facf600a376844b38fe3c10b9e7
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-07-09 12:26:52 -0600

    Footer add bio start

---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+export default function Footer() {
+    const current_year = new Date().getFullYear();
+    return (
+        <div id="section_footer">
+            <div className='flex justify-center space-x-8'>
+                <Link href="" target="_blank" className='btn btn-link'>
+                Features
+                </Link>
+                <span> | </span>
+                <Link href="" target="_blank" className='btn btn-link'>
+                About
+                </Link>
+                <span> | </span>
+                <Link href="" target="_blank" className='btn btn-link'>
+                Testimonials
+                </Link>
+                <span> | </span>
+                <Link href="" className='btn btn-link'>
+                Contact
+                </Link>
+            </div>
+            <div className='text-center py-4'>
+                <p>1234 Happy Street, Rexburg, ID 83460, USA</p>
+                <p>{`© ${current_year} HandcraftHaven/Group 5`}</p>
+            </div>
+        </div>
+    );
+}
+
