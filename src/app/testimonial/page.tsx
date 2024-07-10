9258970dbd9201bf0bfa9f6dba691d8ad160ea6e
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-07-10 14:40:54 -0600

    Added linked pages to footer and style

---
+++ src/app/testimonial/page.tsx
@@ -0,0 +1,15 @@
+import Testimonial from '../ui/testimonial';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: "Register",
+    description: "Register for an account at Handcrafted Haven to start buying and selling unique, handcrafted items.",
+};
+
+export default function RegisterPage() {
+    return (
+        <div className="container pb-8 px-8 mx-auto text-center">
+            <Testimonial />
+        </div>
+    );
+}
--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -5,27 +5,25 @@ export default function Footer() {
     const current_year = new Date().getFullYear();
     return (
         <div id="section_footer">
-            <div className='flex justify-center space-x-8'>
-                <Link href="" target="_blank" className='btn btn-link'>
-                Features
-                </Link>
-                <span> | </span>
-                <Link href="" target="_blank" className='btn btn-link'>
+            <div className='bg-gray-300  flex justify-evenly space-x-8'>
+                <Link href="/about" target="_blank" className='btn btn-link hover:font-bold hover:text-gray-400'>
                 About
                 </Link>
                 <span> | </span>
-                <Link href="" target="_blank" className='btn btn-link'>
+                <Link href="/testimonial" target="_blank" className='btn btn-link hover:font-bold hover:text-gray-400'>
                 Testimonials
                 </Link>
                 <span> | </span>
-                <Link href="" className='btn btn-link'>
+                <Link href="mailto:Julie<jlcrowther@byui.edu>" className='btn btn-link hover:font-bold hover:text-gray-400'>
                 Contact
                 </Link>
             </div>
-            <div className='text-center py-4'>
+            <div className='bg-gray-300 flex justify-center space-x-8 mb-4'>
                 <p>1234 Happy Street, Rexburg, ID 83460, USA</p>
+                <span> | </span>
                 <p>{`© ${current_year} HandcraftHaven/Group 6`}</p>
             </div>
+            
         </div>
     );
 }
