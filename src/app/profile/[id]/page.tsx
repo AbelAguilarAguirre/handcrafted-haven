ce234dda288af30ac7cb772916c92db52811bd78
l-gomes-ar <lgaraujo021@gmail.com>
2024-07-12 01:08:07 +0100

    Added test component for checking user logged in inside a profile page: ui/authorise-test.tsx

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,7 +2,7 @@ import ProductsTable from '@/app/ui/profile/table';
 import Pagination from '@/app/ui/profile/pagination';
 import { fetchProductsByUserId } from '@/app/lib/data';
 import { UUID } from 'crypto';
-
+import AuthoriseComponent from '@/app/ui/authorise-test'; // This is just a test component for showing authorisation, delete later
 
 export default async function Page({ params }: { params: { id: UUID } }) {
     const id = params.id;
@@ -10,6 +10,8 @@ export default async function Page({ params }: { params: { id: UUID } }) {
         const products = await fetchProductsByUserId(id);
         return (
             <div className="flex flex-col items-center min-h-[70vh]">
+                {/* Delete this later, this is just an example component checking if user logged in is the owner of profile */}
+                <AuthoriseComponent params={params}/>
                 <div>
                     <div>
                         <ProductsTable products={products ?? []} />
