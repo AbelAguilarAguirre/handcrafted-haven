ddf4c1b182410c641feb9c3a6dba4d6c96930c63
natelandrum <nate178dawg@gmail.com>
2024-07-11 21:21:41 -0600

    Adjust CSS to page

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'next/link';
 
 export default function Footer() {
     const current_year = new Date().getFullYear();
     return (
-        <div id="section_footer">
-            <div className='bg-gray-300  flex justify-evenly space-x-8'>
-                <Link href="/about" target="_blank" className='btn btn-link hover:font-bold hover:text-gray-400'>
-                About
-                </Link>
-                <span> | </span>
-                <Link href="/testimonial" target="_blank" className='btn btn-link hover:font-bold hover:text-gray-400'>
-                Testimonials
-                </Link>
-                <span> | </span>
-                <Link href="mailto:Julie<jlcrowther@byui.edu>" className='btn btn-link hover:font-bold hover:text-gray-400'>
-                Contact
-                </Link>
+        <div id="section_footer" className="bg-white">
+            <div className='grid grid-cols-5 py-4 justify-items-center'>
+                <div className='flex justify-center'>
+                    <Link 
+                    href="/about" 
+                    target="_blank"
+                    className='btn btn-link text-gray-600 hover:font-bold hover:text-gray-800'>
+                        About
+                    </Link>
+                </div>
+                <span className="text-gray-400"> | </span>
+                <div className='flex justify-center'>
+                    <Link 
+                    href="/testimonial" 
+                    target="_blank"
+                    className='btn btn-link text-gray-600 hover:font-bold hover:text-gray-800'
+                    >
+                        Testimonials
+                    </Link>
+                </div>
+                <span className="text-gray-400"> | </span>
+                <div className='flex justify-center'>
+                    <Link 
+                    href="mailto:Julie<jlcrowther@byui.edu>"
+                    className='btn btn-link text-gray-600 hover:font-bold hover:text-gray-800'
+                    >
+                        Contact
+                    </Link>
+                </div>
             </div>
-            <div className='bg-gray-300 flex justify-center space-x-8 mb-4'>
-                <p>1234 Happy Street, Rexburg, ID 83460, USA</p>
-                <span> | </span>
-                <p>{`© ${current_year} HandcraftHaven/Group 6`}</p>
+            <div className='grid grid-cols-3 py-2 mb-4 justify-items-center'>
+                <p className="text-gray-600 text-center">1234 Happy Street, Rexburg, ID 83460, USA</p>
+                <span className="text-gray-400"> | </span>
+                <p className="text-gray-600 text-center">{`© ${current_year} Handcrafted Haven / Group 6`}</p>
             </div>
-            
         </div>
     );
 }
