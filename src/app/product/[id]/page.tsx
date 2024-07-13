64934f2b285a0b7a68f8bd4e08a8219cd6cabc9a
natelandrum <nate178dawg@gmail.com>
2024-07-13 11:14:59 -0600

    Modifications to product page and profile

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,11 +1,18 @@
 import { ProductDetails } from "../../ui/product/product";
 import { UUID } from 'crypto';
 import { fetchProductByProductId } from "../../lib/data";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Product Details",
+  description:
+    "View the details of a product from Handcrafted Haven.",
+};
 
 
 export default async function Page({ params }: { params: { id: UUID } }) {
   const id = params.id;
-  const product = await fetchProductByProductId(params.id);
+  const product = await fetchProductByProductId(id);
   return (
       <ProductDetails product={product} />
     );
