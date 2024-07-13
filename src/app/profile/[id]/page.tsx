bf67694b7f08b1e680577309e43740464dc38a1a
natelandrum <nate178dawg@gmail.com>
2024-07-13 11:12:22 -0600

    Add metadata and session

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,8 +3,22 @@ import Pagination from '@/app/ui/profile/pagination';
 import { fetchProductsByUserId } from '@/app/lib/data';
 import { UUID } from 'crypto';
 import AuthoriseComponent from '@/app/ui/authorise-test'; // This is just a test component for showing authorisation, delete later
+import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: "Product Details",
+  description:
+    "Login to your Handcrafted Haven account to start buying and selling unique, handcrafted items.",
+};
+
 
 export default async function Page({ params }: { params: { id: UUID } }) {
+    const session = await getServerSession();
+    if (!session) {
+        redirect("/login");
+    }
     const id = params.id;
     try {
         const products = await fetchProductsByUserId(id);
