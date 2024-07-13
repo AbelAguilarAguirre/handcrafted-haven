37a7e3a45b0c741facb670370332290d9b6c6f4a
natelandrum <nate178dawg@gmail.com>
2024-07-13 11:14:16 -0600

    More session and cart context

--- src/app/ui/product/product.tsx
+++ src/app/ui/product/product.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import Rating from "@mui/material/Rating";
 import Image from "next/image";
 import { Product } from "../../lib/definitions";
+import { addToCart } from "../../lib/actions";
+import { useSession } from "next-auth/react";
+import { UUID } from "crypto";
+import { useCart } from "@/app/CartContext";
+
 
 export function ProductDetails({ product }: { product: Product }) {
+  const { data: session } = useSession();
+  let user_id: UUID;
+  if (session) {
+    user_id = session.user.id;
+  }
+  const { cartItemCount, updateCartItemCount } = useCart();
   return (
     <div className="flex flex-col md:flex-row md:space-x-8 p-8 items-center justify-center text-center md:text-left">
       <div className="w-full md:w-1/2 mb-4 md:mb-0">
@@ -22,11 +35,23 @@ export function ProductDetails({ product }: { product: Product }) {
           precision={0.5}
           readOnly
         />
-        {/* Continue with the rest of your component */}
         <p className="text-xl my-2">${product.price}</p>
         <p className="my-2">{product.description}</p>
 
-        <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        <button
+          onClick={() => {
+            if (session) {
+              addToCart(product.product_id, user_id);
+              updateCartItemCount(Number(cartItemCount) + 1);
+            }
+            else {
+              alert("Please login to add items to your cart");
+            }
+          }
+          }
+          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+        >
+
           Add to Cart
         </button>
       </div>
