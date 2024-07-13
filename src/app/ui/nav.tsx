c9e25715d9048aa350822941d2835ba729ca371b
natelandrum <nate178dawg@gmail.com>
2024-07-13 11:14:35 -0600

    Add cart to nav

--- src/app/ui/nav.tsx
+++ src/app/ui/nav.tsx
@@ -1,49 +1,76 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import Cart from "./cart";
 import { signOut, useSession } from 'next-auth/react';
+import { useCart } from "@/app/CartContext";
+import { useEffect } from "react";
+import { getCartItemCount } from "@/app/lib/data";
+
 
 export default function Nav() {
-    const { data: session } = useSession();
+  const { data: session } = useSession();
+    const { cartItemCount, updateCartItemCount } = useCart();
+      useEffect(() => {
+        // This function could be async if getCartItemCount is an async operation
+        const updateCount = async () => {
+          if (session) {
+            // Assuming getCartItemCount returns a Promise, await its result
+            const count = await getCartItemCount(session.user.id);
+            updateCartItemCount(count);
+          } else {
+            // Reset cart item count if there's no session
+            updateCartItemCount(0);
+          }
+        };
 
-    return (
-        <>
-            <nav className="">
-                <ul className="flex flex-wrap justify-around items-center ">
-                    <li className="mx-2">
-                        <Link className="hover:underline" href="/">
-                            Shop
-                        </Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link className="hover:underline" href="/">
-                            About Us
-                        </Link>
-                    </li>
-                    <li className="mx-2">
-                        <Link className="hover:underline" href="/">
-                            Cart
-                        </Link>
-                    </li>
-                    {(session) ? (
-                        <>
-                            <li className="mx-2">
-                                <Link className="hover:underline" href={`/profile/${session?.user?.id}`} title="My Profile">
-                                    {session?.user?.name}
-                                </Link>
-                            </li>
-                            <li className="mx-2">
-                                <button onClick={async () => signOut()}>Logout</button>
-                            </li>
-                        </>
-                    ) : (
-                        <Link className="hover:underline" href="/login">
-                            Log In
-                        </Link>
-                    )}
-                </ul>
-            </nav>
-        </>
-    );
+        updateCount();
+      }, [session]);
+  return (
+    <>
+      <nav className="">
+        <ul className="flex flex-wrap justify-around items-center ">
+          <li className="mx-2">
+            <Link className="hover:underline" href="/">
+              Shop
+            </Link>
+          </li>
+          <li className="mx-2">
+            <Link className="hover:underline" href="/">
+              About Us
+            </Link>
+          </li>
+          {session ? (
+            <>
+              <li className="mx-2">
+                <Link
+                  className="hover:underline"
+                  href={`/profile/${session?.user?.id}`}
+                  title="My Profile"
+                >
+                  {session?.user?.name}
+                </Link>
+              </li>
+              <li className="mx-2">
+                <button onClick={async () => signOut()}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <Link className="hover:underline mx-2" href="/login">
+              Log In
+            </Link>
+          )}
+          {session ? (
+            <li>
+              <Link
+                href={`/cart/${session?.user?.id}`}
+              >
+                <Cart itemCount={cartItemCount} />
+              </Link>
+            </li>
+          ) : null}
+        </ul>
+      </nav>
+    </>
+  );
 }
-
